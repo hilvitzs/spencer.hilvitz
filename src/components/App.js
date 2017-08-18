3ce6b56f8e5c180549f67da6ab6b8e00df53b970
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2017-08-18 15:53:22 -0600

    Add navlink to logo

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, NavLink } from 'react-router-dom';
 import { Navbar } from './Navbar';
 import { Portfolio } from './Portfolio';
 import { Contact } from './Contact';
 import { About } from './About';
 import { Resume } from './Resume';
+import { Main } from './Main';
 
 export const App = () => {
   return (
     <section className='app-wrapper'>
       <section className='heading-section'>
         <div className='heading-wrapper'>
-          <img className='logo' alt='logo' src={require('../logo.png')} />
+          <NavLink to='/'><img className='logo' alt='logo' src={require('../logo.png')} /></NavLink>
           <Navbar />
         </div>
       </section>
       <section className='main-content-section'>
         <Switch>
+          <Route exact path='/' render={() => <Main />} />
           <Route path='/portfolio' render={() => <Portfolio />} />
           <Route path='/contact' render={() => <Contact />} />
           <Route path='/about' render={() => <About />} />
---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Main = () => {
+  return (
+    <div className='main-content-div'>
+      <section className='main-section'>
+        <h1>Spencer Hilvitz</h1>
+        <h2>Front End Web Developer</h2>
+      </section>
+    </div>
+  );
+};
