9366bacd27c239c3d2920beb649159ead78323e0
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2017-08-22 17:46:57 -0600

    start adding portfolio info

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,7 +4,13 @@ export const Portfolio = () => {
   return (
     <div className='main-content-div'>
       <div className='portfolio-div'>
-        This is my portfolio
+        <div className='movie-div'>
+          <div className='portfolio-text'>
+            <p>Movie Tracker</p>
+            <p>React, React Router, Redux</p>
+            <img alt='' className='' src={require('../movietracker.png')} />
+          </div>
+        </div>
       </div>
     </div>
   );
