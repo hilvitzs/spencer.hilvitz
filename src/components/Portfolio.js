3bb0457c322e1fe06862f1e0cbbb0bb20816f545
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2017-08-22 20:22:15 -0600

    further style portfolio page

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,10 +4,18 @@ export const Portfolio = () => {
   return (
     <div className='main-content-div'>
       <div className='portfolio-div'>
+        <div className='mentr-div'>
+          <div className='portfolio-text'>
+            <img alt='' className='' src={require('../mentr.png')} />
+          </div>
+        </div>
         <div className='movie-div'>
           <div className='portfolio-text'>
-            <p>Movie Tracker</p>
-            <p>React, React Router, Redux</p>
+            <img alt='' className='movietracker-img' src={require('../movietracker.png')} />
+          </div>
+        </div>
+        <div className='snake-div'>
+          <div className='portfolio-text'>
             <img alt='' className='' src={require('../movietracker.png')} />
           </div>
         </div>
