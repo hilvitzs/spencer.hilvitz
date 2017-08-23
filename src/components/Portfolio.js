a74a474d6f7531fd39216c168422e5c6057c45ed
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2017-08-22 20:36:24 -0600

    fix portfolio sizing

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,17 +6,17 @@ export const Portfolio = () => {
       <div className='portfolio-div'>
         <div className='mentr-div'>
           <div className='portfolio-text'>
-            <img alt='' className='' src={require('../mentr.png')} />
+            <img alt='' className='mentr-img portfolio-img' src={require('../mentr.png')} />
           </div>
         </div>
         <div className='movie-div'>
           <div className='portfolio-text'>
-            <img alt='' className='movietracker-img' src={require('../movietracker.png')} />
+            <img alt='' className='movietracker-img portfolio-img' src={require('../movietracker.png')} />
           </div>
         </div>
         <div className='snake-div'>
           <div className='portfolio-text'>
-            <img alt='' className='' src={require('../movietracker.png')} />
+            <img alt='' className='snake-img portfolio-img' src={require('../movietracker.png')} />
           </div>
         </div>
       </div>
